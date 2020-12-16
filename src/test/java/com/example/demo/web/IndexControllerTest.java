ef23c36b25d2563062a8ac7826c833f1e9450343
jongwook-song <jssongjk637@gmail.com>
2020-12-16 23:01:24 +0900

    ci

--- src/test/java/com/example/demo/web/IndexControllerTest.java
+++ src/test/java/com/example/demo/web/IndexControllerTest.java
@@ -21,6 +21,7 @@ public class IndexControllerTest {
     public void 메인페이지_로딩() {
         //when
         String body = this.restTemplate.getForObject("/", String.class);
+        
         //then
         assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
     }
